047bb7506757d9f78dc41c3550fe0035003c5c95
aqua <148691880+dpvoda@users.noreply.github.com>
2023-10-22 17:18:43 +0200

    Added error handling and input validation

--- arch/x86/context.c
+++ arch/x86/context.c
@@ -4,27 +4,46 @@
 context_t *save_context;
 context_t *load_context;
 
+#define CONTEXT_REGISTER_COUNT (sizeof(context_t) / sizeof(uint64_t))
+
 void __context_save(uint64_t *stack_top)
 {
-	int regnum = sizeof(context_t) >> 3;
-	uint64_t *save_location = (uint64_t *)save_context;
-	for (int i = 0; i < regnum + 1; i ++)
-	{
-		if (i == 17)
-			stack_top ++;
-		else
-			*save_location++ = *stack_top++;
-	}
+    if (save_context == NULL || stack_top == NULL) {
+        return;
+    }
+
+    uint64_t *save_location = (uint64_t *)save_context;
+
+    for (int i = 0; i <= CONTEXT_REGISTER_COUNT; i++)
+    {
+        if (i == 17)
+        {
+            stack_top++;
+        }
+        else
+        {
+            *save_location++ = *stack_top++;
+        }
+    }
 }
+
 void __context_load(uint64_t *stack_top)
 {
-	int regnum = sizeof(context_t) >> 3;
-	uint64_t *save_location = (uint64_t *)load_context;
-	for (int i = 0; i < regnum + 1; i ++)
-	{
-		if (i == 17) 
-			stack_top++;
-		else
-			*stack_top++ = *save_location++;
-	}
+    if (load_context == NULL || stack_top == NULL) {
+        return;
+    }
+
+    uint64_t *load_location = (uint64_t *)load_context;
+
+    for (int i = 0; i <= CONTEXT_REGISTER_COUNT; i++)
+    {
+        if (i == 17)
+        {
+            stack_top++;
+        }
+        else
+        {
+            *stack_top++ = *load_location++;
+        }
+    }
 }
