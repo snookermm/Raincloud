352eb113a22b8faabf0d8f5760e50ce41d1c2a56
aqua <148691880+dpvoda@users.noreply.github.com>
2023-10-22 17:20:58 +0200

    Refactored syscall initialization

--- syscall/syscall.c
+++ syscall/syscall.c
@@ -2,26 +2,37 @@
 #include <klibc.h>
 #include <mm.h>
 #include <paging.h>
+
 uint64_t *syscall_table = (uint64_t *)SYSCALL_BASE;
 
-void syscall_init(void)
-{
-	printf("Syscall initializing...\n");
-	uint64_t temp = page_alloc(SYS_CALL_TABLE_SIZE);
-	assert(temp != INVALID_ADDR);
-	modify_chunk_mapping(get_current_plm4e(), SYSCALL_BASE, (uint64_t)temp, SYS_CALL_TABLE_SIZE, DEFAULT_PTE_FLAG);
-	memset(syscall_table, 0, SYS_CALL_TABLE_SIZE << 12);
-	
-	*(syscall_table + SYS_syscall) = (uint64_t)&sys_syscall;
-	*(syscall_table + SYS_exit) = (uint64_t)&sys_exit;
-	*(syscall_table + SYS_fork) = (uint64_t)&sys_fork;
-	*(syscall_table + SYS_read) = (uint64_t)&sys_read;
-	*(syscall_table + SYS_write) = (uint64_t)&sys_write;
-	*(syscall_table + SYS_open) = (uint64_t)&sys_open;
-	*(syscall_table + SYS_close) = (uint64_t)&sys_close;
-	*(syscall_table + SYS_exec) = (uint64_t)&sys_exec;
-	*(syscall_table + SYS_lseek) = (uint64_t)&sys_lseek;
-	*(syscall_table + SYS_fstat) = (uint64_t)&sys_fstat;
-	*(syscall_table + SYS_ioctl) = (uint64_t)&sys_ioctl;
-	printf("Syscall initializing complete.\n");
+void set_syscall(uint64_t syscall_number, void *function_ptr) {
+    if (syscall_number >= SYS_CALL_TABLE_SIZE) {
+        return;
+    }
+    syscall_table[syscall_number] = (uint64_t)function_ptr;
+}
+
+void syscall_init(void) {
+    printf("Syscall initializing...\n");
+
+    uint64_t *syscall_table_memory = (uint64_t *)page_alloc(SYS_CALL_TABLE_SIZE);
+    assert(syscall_table_memory != INVALID_ADDR);
+
+    modify_chunk_mapping(get_current_plm4e(), SYSCALL_BASE, (uint64_t)syscall_table_memory, SYS_CALL_TABLE_SIZE, DEFAULT_PTE_FLAG);
+
+    memset(syscall_table_memory, 0, SYS_CALL_TABLE_SIZE * PAGE_SIZE);
+
+    set_syscall(SYS_syscall, &sys_syscall);
+    set_syscall(SYS_exit, &sys_exit);
+    set_syscall(SYS_fork, &sys_fork);
+    set_syscall(SYS_read, &sys_read);
+    set_syscall(SYS_write, &sys_write);
+    set_syscall(SYS_open, &sys_open);
+    set_syscall(SYS_close, &sys_close);
+    set_syscall(SYS_exec, &sys_exec);
+    set_syscall(SYS_lseek, &sys_lseek);
+    set_syscall(SYS_fstat, &sys_fstat);
+    set_syscall(SYS_ioctl, &sys_ioctl);
+
+    printf("Syscall initializing complete.\n");
 }
